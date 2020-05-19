47fc6f2ba7f2639968891c77f2055376f0bf7624
Owlspec3086 <ethical.worker3@gmail.com>
2020-05-19 00:02:00 -0400

    flex assignment working

--- client/src/App.js
+++ client/src/App.js
@@ -1,7 +1,12 @@
 import React, { useState, useEffect } from 'react';
 import axios from 'axios';
+//install react-router-dom then import it here BrowserRouter as Router, Route, Link
+import { BrowserRouter as Router, Route, Link } from 'react-router-dom';
 
 import SavedList from './Movies/SavedList';
+// import movie list and movie here
+import MovieList from './Movies/MovieList';
+import Movie from './Movies/MovieList';
 
 const App = () => {
   const [savedList, setSavedList] = useState([]);
@@ -27,8 +32,15 @@ const App = () => {
 
   return (
     <div>
+      {/* Wrapping in router & adding */}
+      <Router>
       <SavedList list={savedList} />
-      <div>Replace this Div with your Routes</div>
+
+      {/* Placing "extract" on route to make it go to the specific path
+      then using the regular path to make it go to movie */}
+      <Route extract path='/' component={MovieList} />
+      <Route path='/movie/:id' component={Movie} />
+      </Router>
     </div>
   );
 };
